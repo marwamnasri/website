71c670d8e1a8a8602caa3761f0f3b2a614f35949
marwa mnasri <marwamnasri75@gmail.com>
2023-08-27 17:52:59 +0200

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,40 @@
+const DATA = [
+    {
+        id : 0,
+        title : "Boîte avec couvercle",
+        price : 350,
+        desc : "Boîte avec couvercle, 3 pièces Les variations naturelles de couleur du jonc de mer donnent à chaque panier son aspect unique.Chaque boîte a été fabriquée à la main, ce qui la rend unique.Dimensions:– 1 pièce diamètre 10 cm, H6 cm;– 1 pièce diamètre 15 cm, H8 cm;– 1 pièce diamètre 20 cm, H10 cm.",
+        img : "/assets/images/products/img1.jpg"
+    },
+    {
+        id : 0,
+        title : "Étagère -Blanc – L60 x P20 x H160 cm",
+        price : 550,
+        desc : "Couleur : blanc Matière : panneaux MDF Dimensions : L60 x P20 x H160 cm",
+        img : "/assets/images/products/img5.jpg"
+    },
+    {
+        id : 1,
+        title : "Vélo Panier De Fleurs",
+        price : 150,
+        desc : "Vélo Panier De Fleurs Pour la décoration de la maison, les arrangements de mariage, les arrangements de fête, et la décoration de scène, décoration de table, etc. Matériel: Rotin et plastique Taille: 23cm (longueur totale) 12,5cm (panier de fleurs diamètre)9cm (hauteur du panier)1 x panier de design pour vélo",
+        img :"/assets/images/products/img2.jpg"
+    },
+    {
+        id : 2,
+        title : "Horloge Murale PLATINET SUNSET",
+        price : 250,
+        desc : "Horloge Murale PLATINET SUNSET – 12 cadres photo – Grand nombre – Matériel: Plastique de haute qualité – Alimentation: 1 x pile AA.",
+        img : "/assets/images/products/img3.jpg"
+    },
+    {
+        id : 3,
+        title : "lampe tripiers Scandinave",
+        price : 1050,
+        desc : "lampe tripiers Scandinave Matière : Bois /Longueur : 20 cm / Largeur : 20 cm /Hauteur : 45.5 cm.",
+        img : "/assets/images/products/img4.jpg"
+    },
+    
+]
+
+export default DATA;
